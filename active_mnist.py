32582a67cd94345ba54c5c004d992b19b749412f
Tim Wiesner <62284314+TimWiesner99@users.noreply.github.com>
2022-02-09 16:15:08 +0100

    Improved `plot_example()` function
    Can now add a "sup"-title to the figure. Distribution over classes is now also plotted to better illustrate distractor samples.

--- active_mnist.py
+++ active_mnist.py
@@ -25,28 +25,41 @@ def to_one_hot(y, dims=10, dtype=torch.FloatTensor):
     return out
 
 
-def plot_example(original_images, transformed_images, labels, transforms, number_examples=1):
+def plot_example(original_images, transformed_images, labels, transforms, number_examples=1, title=''):
     assert len(original_images) == len(transformed_images) == len(labels) == len(transforms)
     assert len(original_images) >= number_examples
 
     for i in range(number_examples):
-        fig, axs = plt.subplots(ncols=2, figsize=(6, 3))
+        fig, axs = plt.subplots(ncols=3, figsize=(9, 3))
         axs[0].imshow(flatten_channel_dim(original_images)[i], cmap=cm.gray_r)
         axs[0].set_title('Original MNIST Figure')
-        axs[1].imshow(flatten_channel_dim(transformed_images)[i], cmap=cm.gray_r)
-        axs[1].set_title('Transformed MNIST Figure')
         axs[0].set_xticks([])
         axs[0].set_yticks([])
+        axs[1].imshow(flatten_channel_dim(transformed_images)[i], cmap=cm.gray_r)
+        axs[1].set_title('Transformed MNIST Figure')
         axs[1].set_xticks([])
         axs[1].set_yticks([])
+        axs[2].bar(range(10), labels[i])
+        axs[2].set_xticks(range(10))
+        axs[2].set_xlabel('Digit')
+        axs[2].set_ylim(0, 1)
+        axs[2].set_title('Distribution over Class')
 
-        label = torch.where(labels[i] == 1)[0]
+        fig.subplots_adjust(top=0.8)
 
-        description = f'Label = {label.data.cpu().numpy()[0]}\n' \
+        fig.suptitle(title + '\n', fontsize=16, y=0.98)
+
+        if torch.max(labels[i]) < 1:
+            label = "?"
+        else:
+            label = torch.argmax(labels[i])
+
+        description = f'Label = {label}\n' \
                       f'Scale = {transforms[i, 0].data.cpu().numpy():.3f}x {transforms[i, 1].data.cpu().numpy():.3f}y\n' \
                       f'Rotation = {transforms[i, 2].data.cpu().numpy() / math.pi:.3f}π\n' \
                       f'Translation = {transforms[i, 3].data.cpu().numpy():.3f}x {transforms[i, 4].data.cpu().numpy():.3f}y'
         plt.text(0.95, 0.5, description, fontsize=14, transform=plt.gcf().transFigure)
+
         plt.show()
 
 
@@ -99,7 +112,7 @@ class Active_MNIST:
         self.mean_rotation = 0
         self.std_rotation = math.pi / 2
         self.mean_translation = 0
-        self.std_translation = 0.25
+        self.std_translation = 10/self.size
 
         self.or_img = None
         self.trns_img = None
@@ -115,7 +128,8 @@ class Active_MNIST:
             torch.cuda.empty_cache()
 
         print(f'Sampling {self.batch_size * self.max_iterations} images...')
-        print(f' {(1 - (perc_normal + perc_distractors)) * 100}% transformed\n {(perc_distractors) * 100}% distractors\n {(perc_normal) * 100}% normal.')
+        print(
+            f' {(1 - (perc_normal + perc_distractors)) * 100}% transformed\n {(perc_distractors) * 100}% distractors\n {(perc_normal) * 100}% normal.')
         self.or_img, self.trns_img, self.lbl, self.trns = self.sample(n=self.batch_size * self.max_iterations,
                                                                       perc_normal=perc_normal,
                                                                       perc_distractors=perc_distractors)
@@ -131,7 +145,7 @@ class Active_MNIST:
         self.mean_rotation = mean
         self.std_rotation = std
 
-    def set_translation(self, mean=0, std=10):
+    def set_translation(self, mean=0, std=10/56):
         self.mean_translation = mean
         self.std_translation = std
 
@@ -168,7 +182,7 @@ class Active_MNIST:
 
     def sample(self, n=1, perc_normal=0.0, perc_distractors=0.0):
         assert 0.0 <= perc_normal + perc_distractors <= 1, \
-            'Percentage of normal/untransformed + distractor images must be between 0 and 1!'
+            'Percentage of non-transformed + distractor images must be between 0 and 1!'
 
         def transformation_matrix(transform=[1, 1, 0, 0, 0]):
             # scale
@@ -192,7 +206,7 @@ class Active_MNIST:
 
         n_normal = int(n * perc_normal)
         n_distractors = int(n * perc_distractors)
-        n_transformed = int(n - (n_normal + perc_distractors))
+        n_transformed = int(n - (n_normal + n_distractors))
 
         print('     Sampling indices')
         index_trns = np.random.choice(len(self.labels), size=n_transformed, replace=True)
@@ -220,11 +234,11 @@ class Active_MNIST:
         if n_distractors > 0:
             scales = torch.Tensor(np.abs(np.random.normal(loc=1, scale=0.25,
                                                           size=(n_distractors, 2)))).type(self.dtype)
-            rotations = torch.Tensor((np.random.normal(loc=0, scale=math.pi/2,
+            rotations = torch.Tensor((np.random.normal(loc=0, scale=math.pi / 2,
                                                        size=(n_distractors, 1))
                                       % math.pi)
                                      * np.random.choice([-1, 1])).type(self.dtype)
-            translations = torch.Tensor(np.random.normal(loc=0, scale=10,
+            translations = torch.Tensor(np.random.normal(loc=0, scale=10/self.size,
                                                          size=(n_distractors, 2))).type(self.dtype)
 
             trns_dist = torch.cat((scales, rotations, translations), dim=1).type(self.dtype)
@@ -244,9 +258,8 @@ class Active_MNIST:
                     torch.randn(size=[n_distractors, 10]).type(self.dtype),
                     to_one_hot(torch.Tensor(self.labels[index_dist]).type(self.dtype), dtype=self.dtype),
                     alpha=2),
-                10)/10
+                10) / 10
             , dim=1)  # makes class distribution mostly flat with a bit of noise and a small peak at true label
-
 
         selected_imgs_normal = self.images[index_normal].type(self.dtype)
         lbl_normal = to_one_hot(torch.Tensor(self.labels[index_normal]).type(self.dtype), dtype=self.dtype)
@@ -279,7 +292,6 @@ class Active_MNIST:
                 trns_img_dist = func.grid_sample(or_img_dist, grid).type(self.dtype)
         else:
             trns_img_dist = torch.zeros((n_distractors, 1, self.size, self.size)).type(self.dtype)
-
 
         trns_img_normal = torch.clone(or_img_normal).type(self.dtype)
 
